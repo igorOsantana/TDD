8a902732800506fad25de5f56edfbb3e0c8fc627
Igor <igorsantana33231011@gmail.com>
2022-02-01 14:48:44 -0300

    feat: ensure SignUpController returns an error if email is not provided by request body

--- src/presetation/controllers/signup.ts
+++ src/presetation/controllers/signup.ts
@@ -1,8 +1,17 @@
 export class SignUpController {
   handle(httpReq: any): any {
-    return {
-      statusCode: 400,
-      body: new Error('Missing name in request body'),
-    };
+    if (!httpReq.body.name) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing "name" in request body'),
+      };
+    }
+
+    if (!httpReq.body.email) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing "email" in request body'),
+      };
+    }
   }
 }
