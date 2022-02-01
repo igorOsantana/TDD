428d7b69969a2eb9950da357a639b8f23389c450
Igor <igorsantana33231011@gmail.com>
2022-02-01 14:48:52 -0300

    test: ensure SignUpController returns an error if email is not provided by request body

--- src/presetation/controllers/signup.spec.ts
+++ src/presetation/controllers/signup.spec.ts
@@ -12,6 +12,20 @@ describe('Sign Up Controller', () => {
     };
     const httpRes = sut.handle(httpReq);
     expect(httpRes.statusCode).toBe(400);
-    expect(httpRes.body).toEqual(new Error('Missing name in request body'));
+    expect(httpRes.body).toEqual(new Error('Missing "name" in request body'));
+  });
+
+  test('Should return 400 if email is not provided by request body', () => {
+    const sut = new SignUpController();
+    const httpReq = {
+      body: {
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password',
+      },
+    };
+    const httpRes = sut.handle(httpReq);
+    expect(httpRes.statusCode).toBe(400);
+    expect(httpRes.body).toEqual(new Error('Missing "email" in request body'));
   });
 });
