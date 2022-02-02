0a028ded81394cac44ec01a2d86e608cf2ca6ffe
Igor <igorsantana33231011@gmail.com>
2022-02-02 12:09:09 -0300

    feat: ensure DbAddAccount calls Encrypter with correct password

---
+++ src/data/usecases/addAccount/dbAddAccount.ts
@@ -0,0 +1,19 @@
+import {
+  AddAccount,
+  AddAccountModel,
+} from '../../../domain/usecases/addAccount';
+import { AccountModel } from '../../../domain/models/account';
+import { Encrypter } from '../../protocols';
+
+export class DbAddAccount implements AddAccount {
+  private readonly encrypter: Encrypter;
+
+  constructor(encrypter: Encrypter) {
+    this.encrypter = encrypter;
+  }
+
+  async add(account: AddAccountModel): Promise<AccountModel> {
+    await this.encrypter.encrypt(account.password);
+    return new Promise(resolve => resolve(null));
+  }
+}
