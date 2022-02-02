24b6643f44ecd278df05c0789fb63dd96a3013f2
Igor <igorsantana33231011@gmail.com>
2022-02-02 12:26:36 -0300

    test: ensure DbAddAccount throws if Encrypter throws

--- src/data/usecases/addAccount/dbAddAccount.spec.ts
+++ src/data/usecases/addAccount/dbAddAccount.spec.ts
@@ -35,4 +35,33 @@ describe('DbAddAccount Usecase', () => {
     await sut.add(account);
     expect(encryptSpy).toHaveBeenCalledWith('valid_password');
   });
+
+  test('Should throw if Encrypter throws', async () => {
+    const { sut, encrypterStub } = makeSut();
+    jest.spyOn(encrypterStub, 'encrypt').mockImplementationOnce(async () => {
+      return new Promise((_, reject) => reject(new Error()));
+    });
+    const account = {
+      name: 'valid_name',
+      email: 'valid_email@mail.com',
+      password: 'valid_password',
+    };
+    const promise = sut.add(account);
+    await expect(promise).rejects.toThrow();
+  });
+
+  // test('Should returns 500 if Encrypter throws', async () => {
+  //   const { sut, encrypterStub } = makeSut();
+  //   jest.spyOn(encrypterStub, 'encrypt').mockImplementationOnce(async () => {
+  //     return new Promise((_, reject) => reject(new Error()));
+  //   });
+  //   const account = {
+  //     name: 'valid_name',
+  //     email: 'valid_email@mail.com',
+  //     password: 'valid_password',
+  //   };
+  //   const httpRes = await sut.add(account);
+  //   expect(httpRes.statusCode).toBe(500);
+
+  // });
 });
