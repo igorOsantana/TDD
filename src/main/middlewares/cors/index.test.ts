340fd9bbe3bbf18ecf51ac137cce31023c30af78
Igor <igorsantana33231011@gmail.com>
2022-02-03 12:00:51 -0300

    test: ensure requests returns json as default content type

--- src/main/middlewares/cors/index.test.ts
+++ src/main/middlewares/cors/index.test.ts
@@ -3,10 +3,10 @@ import app from '../../config/app';
 
 describe('Cors Middleware', () => {
   test('Should enable cors', async () => {
-    app.get('/test/cors', (req, res) => res.send());
+    app.get('/test/cors', (_req, res) => res.send());
 
     await request(app)
-      .get('/test/body-parser')
+      .get('/test/cors')
       .expect('access-control-allow-origin', '*')
       .expect('access-control-allow-methods', '*')
       .expect('access-control-allow-headers', '*');
