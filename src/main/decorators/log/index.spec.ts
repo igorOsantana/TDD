44c3358cc055d08f728f49f73865bdbfe251cb47
Igor <igorsantana33231011@gmail.com>
2022-02-07 10:40:23 -0300

    test: ensure LogMongoRepository creates an error log on success

--- src/main/decorators/log/index.spec.ts
+++ src/main/decorators/log/index.spec.ts
@@ -20,7 +20,7 @@ const makeControllerStub = (): Controller => {
 
 const makeLogErrorRepository = (): LogErrorRepository => {
   class LogErrorRepositoryStub implements LogErrorRepository {
-    async log(stack: string): Promise<void> {
+    async logError(stack: string): Promise<void> {
       return new Promise(resolve => resolve(null));
     }
   }
@@ -83,7 +83,7 @@ describe('Log Controller Decorator', () => {
 
   test('Should call LogErrorRepository with correct error if controller returns a server error', async () => {
     const { sut, controllerStub, logErrorRepositoryStub } = makeSut();
-    const logSpy = jest.spyOn(logErrorRepositoryStub, 'log');
+    const logSpy = jest.spyOn(logErrorRepositoryStub, 'logError');
     jest
       .spyOn(controllerStub, 'handle')
       .mockReturnValueOnce(Promise.resolve(makeFakeServerError()));
