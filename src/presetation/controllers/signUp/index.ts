1d27c4b1827077493abac2aa753b19b6590c0c57
Igor <igorsantana33231011@gmail.com>
2022-02-07 17:00:22 -0300

    refactor: remove unused code

--- src/presetation/controllers/signUp/index.ts
+++ src/presetation/controllers/signUp/index.ts
@@ -5,7 +5,6 @@ import {
   EmailValidator,
   AddAccount,
 } from './protocol';
-import { InvalidParamError } from '../../errors';
 import { badRequest, internalServerError, ok } from '../../helpers/http';
 import { Validation } from '../../helpers/validators/validation';
 export class SignUpController implements Controller {
@@ -19,22 +18,12 @@ export class SignUpController implements Controller {
   }
 
   async handle(httpReq: HttpRequest): Promise<HttpResponse> {
-    const requiredFields = [
-      'name',
-      'email',
-      'password',
-      'passwordConfirmation',
-    ];
     try {
       const error = this.validation.validate(httpReq.body);
       if (error) {
         return badRequest(error);
       }
       const { name, email, password } = httpReq.body;
-
-      if (password.length < 6) {
-        return badRequest(new InvalidParamError('password'));
-      }
 
       const account = await this.addAccount.add({ name, email, password });
       return ok(account);
