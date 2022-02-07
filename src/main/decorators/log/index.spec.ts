553da891dff470e70d702cad05ef475d1d35a73a
Igor <igorsantana33231011@gmail.com>
2022-02-07 08:43:11 -0300

    test: ensure LogControllerDecorator returns the same result of controller

--- src/main/decorators/log/index.spec.ts
+++ src/main/decorators/log/index.spec.ts
@@ -39,7 +39,7 @@ describe('Log Controller Decorator', () => {
   test('Should call controller handle method', async () => {
     const { sut, controllerStub } = makeSut();
     const handleSpy = jest.spyOn(controllerStub, 'handle');
-    const httpRes = {
+    const httpReq = {
       body: {
         name: 'any_name',
         email: 'any_mail@mail.com',
@@ -47,7 +47,29 @@ describe('Log Controller Decorator', () => {
         passwordConfirmation: 'any_password',
       },
     };
-    await sut.handle(httpRes);
-    expect(handleSpy).toHaveBeenCalledWith(httpRes);
+    await sut.handle(httpReq);
+    expect(handleSpy).toHaveBeenCalledWith(httpReq);
+  });
+
+  test('Should return the same result of the controller', async () => {
+    const { sut } = makeSut();
+    const httpReq = {
+      body: {
+        name: 'any_name',
+        email: 'any_mail@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password',
+      },
+    };
+    const httpRes = await sut.handle(httpReq);
+    expect(httpRes).toEqual({
+      statusCode: 200,
+      body: {
+        name: 'any_name',
+        email: 'any_mail@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password',
+      },
+    });
   });
 });
